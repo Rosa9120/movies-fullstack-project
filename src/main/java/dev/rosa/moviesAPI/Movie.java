6785cdf69ffdb920f3077f4bfc5c8dffa018f133
rmrl3 <rmrl3@alu.ua.es>
2023-12-10 19:56:51 +0100

    Created classes Movie and Review

---
+++ src/main/java/dev/rosa/moviesAPI/Movie.java
@@ -0,0 +1,29 @@
+package dev.rosa.moviesAPI;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
+
+import java.util.List;
+
+@Document(collection = "movies")
+// lombok
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Movie {
+    private ObjectId id;
+    private String imbdId;
+    private String title;
+    private String releaseDate;
+    private String trailerLInk;
+    private String poster;
+    private List<String> genres;
+    private List<String> backdrops;
+    @DocumentReference  // store only the ids of the review and the reviews will be stored on a different location
+    private List<Review> reviewIds;
+
+}
